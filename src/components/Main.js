b0a9d96155cea2658c61e81e58a06b20c4920dbc
atoneyd <31745478+atoneyd@users.noreply.github.com>
2022-02-19 20:05:23 -0500

    dropdown
    lol

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,7 @@
 import dog from '../assets/wide_dog_cover2_.jpg';
 import React from 'react'
-import {  Button, Checkbox, Form, Message} from 'semantic-ui-react';
+import {  Button, Checkbox, Form, Message, Dropdown} from 'semantic-ui-react';
+
 function Main() {
     const [success, setSuccess] = React.useState("");
     const [error, setError] = React.useState("");
@@ -26,6 +27,31 @@ function Main() {
             // setEmail("");
         });
     };
+    const countryOptions = [
+        { key: 'af', value: 'af', text: 'Afghanistan' },
+        { key: 'ax', value: 'ax', text: 'Aland Islands' },
+        { key: 'al', value: 'al',  text: 'Albania' },
+        { key: 'dz', value: 'dz',  text: 'Algeria' },
+        { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
+        { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
+        { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
+        { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
+        { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
+        { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
+        { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
+        { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
+        { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
+        { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
+        { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
+        { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
+        { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
+        { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
+        { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
+        { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
+        { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
+        { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
+        { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
+      ]
     return(
         <div>
             <img src={dog} className="App-logo" alt="logo" />
@@ -41,7 +67,13 @@ function Main() {
             </Form.Field>
             <Form.Field>
                 <label>Major</label>
-                <input placeholder='Major' />
+                <Dropdown
+    placeholder='Select Major'
+    fluid
+    search
+    selection
+    options={countryOptions}
+  />
             </Form.Field>
             <Form.Field>
                 <Checkbox label='Do you have a minor?' />
